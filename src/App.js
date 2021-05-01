0ebc84e670ba11f6278c451e790efcf849d4b1b8
skkim0303 <skkim0303@gmail.com>
2021-05-01 18:51:39 +0900

    Customer.js 재정의

--- src/App.js
+++ src/App.js
@@ -3,22 +3,52 @@ import Customer from "./components/Customer.js"
 import './App.css';
 
 
-const customer = {
+const customer = [{
+  "id" : "1",
+  "image" : "https://placeimg.com/80/80/1",
   "name" : "박관순",
   "birth" : "123456",
   "gender" : "남자",
   "job" : "축구선수"
+},
+{
+  "id" : "2",
+  "image" : "https://placeimg.com/80/80/2",
+  "name" : "김남규",
+  "birth" : "789016",
+  "gender" : "남자",
+  "job" : "요리사"
+},
+{
+  "id" : "3",
+  "image" : "https://placeimg.com/80/80/3",
+  "name" : "오소리",
+  "birth" : "567388",
+  "gender" : "여자",
+  "job" : "경찰"
 }
+]
 
 class App extends Component {
   render() {
     return(
-     <Customer 
-        name={customer.name}
-        birth={customer.birth}
-        gender={customer.gender}
-        job={customer.job}
-     />
+      <div>
+        {
+          customer.map(c=>{
+            return (
+              <Customer 
+                key={c.id}  
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birth={c.borth}
+                gender={c.gender}
+                job={c.birth}
+              />
+            )
+          })
+        }
+      </div>
     );
   }
 }
